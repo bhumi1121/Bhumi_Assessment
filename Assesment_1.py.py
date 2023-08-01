4882608ad3397f70ea7c03fb24bfbe100dc1da77
bhumi1121 <127817991+bhumi1121@users.noreply.github.com>
2023-08-01 12:18:59 +0530

    Add files via upload

--- Assesment_1.py.py
+++ Assesment_1.py.py
@@ -1,100 +1,114 @@
 def mainmenu():
-    while True:
-        m = '''
-            WELCOME TO TOPS QUIZ GAMINGING CHALLANGE
-            Select your role:
-                        --> Quiz master   (press 1)
-                        --> Quiz cracker  (press 2)
+    m = '''
+        WELCOME TO TOPS QUIZ GAMINGING CHALLANGE
+        Select your role:
+                    --> Quiz master   (press 1)
+                    --> Quiz cracker  (press 2)
 
-           '''
-        print(m)
-        role = input("Enter your role: ")
+       '''
+    print(m)
+    role = input("Enter your role: ")
 
-        if role == "1":
-            print("Welcome master")
-            print("SHAKE YOUR BRAIN AND ADD SOME CHALLANGING QUESTIONS..")
-            question_dict={}
+    if role == "1":
+        print("Welcome master")
+        print("SHAKE YOUR BRAIN AND ADD SOME CHALLANGING QUESTIONS..")
+        question_dict = {}
 
+        while True:
+            a = '''
+                press 1 for add question
+                press 2 for view question
+                press 3 for delete question
+                press 4 for exit
 
-            while True:
-                a = '''
-                    press 1 for add question
-                    press 2 for view question
-                    press 3 for delete question
-                    press 4 for exit
-    
-                   '''
-                print(a)
-                op=input("Which opertion do you want to perform? ")
+               '''
+            print(a)
+            op = input("Which operation do you want to perform? ")
 
-                if op =="1":
-                    qno = int(input("Q.no: "))
-                    que = {}
-                    question_dict={}
-                    q=input("Enter your question: ")
-                    op1=input("Enter option 1: ")
-                    op2=input("Enter option 2: ")
-                    ans=input("Enter the correct answer (1 or 2): ")
-                    que['q']=q
-                    question_dict[qno]=que
-                    print(question_dict)
-                    print("Question added successfully....")
+            if op == "1":
+                qno = int(input("Q.no: "))
+                q = input("Enter your question: ")
+                op1 = input("Enter option 1: ")
+                op2 = input("Enter option 2: ")
+                ans = input("Enter the correct answer (1 or 2): ")
 
-                elif op == '2':
-                    if not question_dict:
-                        print("No question added yet ....")
-                    else:
-                        print("current questions: ")
-                        for qno in question_dict.items():
-                            print("Q{qno}:{que['qestion']}")
-                            print("  1. {que['option 1']}")
-                            print("  2. {que['option 2']}")
-                            print()
-                elif op == '3':
-                    qno = int(input("Enter question number to delete: "))
-                    if qno in question_dict:
-                        del question_dict[qno]
-                        print("Question delete successfully...")
-                    else:
-                        print("Question not found")
+                question_dict[qno] = {
+                    'question': q,
+                    'option 1': op1,
+                    'option 2': op2,
+                    'answer': ans
+                }
+                print(question_dict)
+                print("Question added successfully....")
 
-                elif op == '4':
-                    print("Exit")
-                    break
-
+            elif op == '2':
+                if not question_dict:
+                    print("No question added yet ....")
                 else:
-                    print("invalid option please try agian")
-        elif role == '2':
-              print("Get ready to solve some challanging question")
-        else:
-            print("Invalid role .please enter 1 or 2")
+                    print("Current questions: ")
+                    for qno, que in question_dict.items():
+                        print(f"Q{qno}: {que['question']}")
+                        print(f"  1. {que['option 1']}")
+                        print(f"  2. {que['option 2']}")
+                        print()
 
+            elif op == '3':
+                qno = int(input("Enter question number to delete: "))
+                if qno in question_dict:
+                    del question_dict[qno]
+                    print("Question deleted successfully...")
+                else:
+                    print("Question not found")
 
-score=0
-print("SCORE =",score)
+            elif op == '4':
+                print("Exit")
+                break
 
+            else:
+                print("Invalid option please try again")
 
-d1={
-    'q':q,
-    'op1':op1,
-    'op2':op2,
-    'ans':ans,
-}
-
-
-ans=""
-
-for i,k in d1.items():
-    print(i)
-    ans=input("Enter your answer : ")
-
-    if ans==k:
-        score+=5
+    elif role == '2':
+        print("Welcome quiz cracker")
+        print("Get ready to solve some challenging questions")
     else:
-        score-=10
-
-print("your Score : ",score)
+        print("Invalid role. Please enter 1 or 2")
 
 
 mainmenu()
+def quiz_cracker(questions_dict):
+    score = 0
+    print("SCORE =", score)
 
+    for question, correct_answer in questions_dict.items():
+        print(question)
+        ans = input("Enter your answer: ")
+
+        if ans.lower() == correct_answer.lower():
+            score += 5
+        else:
+            score -= 10
+
+    print("Your Score:", score)
+
+
+d1 = {
+    "Who developed the Python language?": 'guido van rossum',
+    "When was the Python language released?": 'february 20,1991',
+    "Who is the Prime Minister of India?": 'narendra modi',
+    "In which language is Django used?": 'python',
+    "How much population is there in India?": '142cr',
+    "At which position is India placed about population in the whole world?": '1',
+    "Who is the best cricket player in the world?": 'Virat kohli',
+    "Who is the 1st hacker in the world?": 'john draper',
+    "Which thing was hacked 1st in the world?": 'land line',
+    "Who was the 1st person to go into space from India?": 'rakesh sharma'
+}
+
+mainmenu()
+
+role = input("Enter your role: ")
+
+if role == '2':
+    print("Welcome quiz cracker")
+    print("Get ready to solve some challenging questions")
+    quiz_cracker(d1)
